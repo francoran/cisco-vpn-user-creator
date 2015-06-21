bbe547d3bbba93674fe6fd3346c2aa04dc3e382c
francoran <funranco@gmail.com>
2015-06-21 20:18:27 +0300

    Create configurations.py

---
+++ conf/configurations.py
@@ -0,0 +1,3 @@
+ip_list = ['']                # Set the ip address of one or more cisco devices in which the user will be created.
+cisco_username = 'cisco'      # Set the username of your cisco device.
+cisco_password = ''           # Set the password for your cisco device.
